078f2953da2eb24807e67bbd4713d5d7b3130d71
Paul Fennell <paul.fennell3@mail.dcu.ie>
2022-02-01 21:02:05 +0000

    I have added a verifyEmailAddress method which checks for an invalid email address and if there are empty fields. I have also added another button to clear text and reset the saved data to null

--- app/src/main/java/com/example/SDA_ASSIGNMENT4_PAULFENNELL/Settings.java
+++ app/src/main/java/com/example/SDA_ASSIGNMENT4_PAULFENNELL/Settings.java
@@ -27,8 +27,7 @@ public class Settings extends Fragment {
         // Required empty public constructor
     }
 
-//
-    //
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
