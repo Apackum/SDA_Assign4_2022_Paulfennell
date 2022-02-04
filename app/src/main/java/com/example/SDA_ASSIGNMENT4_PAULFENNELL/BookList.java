0f5819973af6efe747e915620b6299458df0aa4f
Paul Fennell <paul.fennell3@mail.dcu.ie>
2022-02-04 13:35:51 +0000

    apk added

--- app/src/main/java/com/example/SDA_ASSIGNMENT4_PAULFENNELL/BookList.java
+++ app/src/main/java/com/example/SDA_ASSIGNMENT4_PAULFENNELL/BookList.java
@@ -26,12 +26,10 @@ import java.util.ArrayList;
 
 
 /**
- * Images used are sourced from Public Domain Day 2019.
- * by Duke Law School's Center for the Study of the Public Domain
- * is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
  * A simple {@link Fragment} subclass.
- * @author Chris Coughlan
+ * @author Paul Fennell
  */
+
 public class BookList extends Fragment {
 
     public BookList() {
@@ -39,7 +37,7 @@ public class BookList extends Fragment {
     }
     //Widgets
     RecyclerView recyclerView;
-    //Firebase
+    //Firebase reference variable
     public DatabaseReference myRef;
     //My variables
     public ArrayList<books> booksList;
@@ -51,11 +49,12 @@ public class BookList extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_book_list, container, false);
+
         recyclerView = root.findViewById(R.id.bookView_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-        //Firebase
+        //Firebase reference
         myRef = FirebaseDatabase.getInstance().getReference();
         //Arraylist
         booksList = new ArrayList<>();
@@ -63,34 +62,38 @@ public class BookList extends Fragment {
         //CLear arrayList
         ClearAll();
 
-        //Getting the data message
+        //Getting the data message method
         getDataFromFirebase();
-return root;
+        return root;
     }
 
-
+    //Methods
 
     private void getDataFromFirebase() {
+        //Creating a query for firebase
         Query query = myRef.child("books");
 
         query.addListenerForSingleValueEvent(new ValueEventListener() {
+
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Calling clearall method
                 ClearAll();
+                //for loop to add to the booklist from database
                 for (DataSnapshot snapshots : dataSnapshot.getChildren()) {
+
                     books Books = new books();
                     Books.setImageUrl(snapshots.child("image").getValue().toString());
                     Log.d("help", "Value is: " + Books);
                     Books.setAuthor(snapshots.child("author").getValue().toString());
                     Books.setTitle(snapshots.child("title").getValue().toString());
-
                     booksList.add(Books);
                 }
+                //Setting up recycler
                 recyclerAdapter = new LibraryViewAdapter(getContext(), booksList);
                 recyclerView.setAdapter(recyclerAdapter);
                 recyclerAdapter.notifyDataSetChanged();
-
             }
 
             @Override
@@ -100,6 +103,8 @@ return root;
             }
         });
     }
+
+    //This is my clearALL method
     private void ClearAll() {
         if (booksList != null) {
             booksList.clear();
@@ -112,34 +117,3 @@ return root;
     }
 }
 
-       /*
-        ArrayList<String> mAuthor = new ArrayList<>();
-        ArrayList<String> mTitle = new ArrayList<>();
-        ArrayList<Integer> mImageID = new ArrayList<>();
-
-        //simple loop here to add the images to the array without typing each one
-        for(int i=1;i<=14;i++) {
-            int id = getResources().getIdentifier("sku1000" + i, "drawable",
-                    root.getContext().getPackageName());
-            mImageID.add(id);
-        }
-
-        //adding author and title.
-        mAuthor.add("Edgar Rice Burroughs"); mTitle.add("Tarzan and the Golden Lion");
-        mAuthor.add("Agatha Christie"); mTitle.add("The Murder on the Links");
-        mAuthor.add("Winston S. Churchill"); mTitle.add("The World Crisis");
-        mAuthor.add("E.e. cummings"); mTitle.add("Tulips and Chimneys");
-        mAuthor.add("Robert Frost"); mTitle.add("New Hampshire");
-        mAuthor.add("Kahlil Gibran"); mTitle.add("The Prophet");
-        mAuthor.add("Aldous Huxley"); mTitle.add("Antic Hay");
-        mAuthor.add("D.H. Lawrence"); mTitle.add("Kangaroo");
-        mAuthor.add("Bertrand and Dora Russell"); mTitle.add("The Prospects of Industrial Civilization");
-        mAuthor.add("Carl Sandberg"); mTitle.add("Rootabaga Pigeons");
-        mAuthor.add("Edith Wharton"); mTitle.add("A Son at the Front");
-        mAuthor.add("P.G. Wodehouse"); mTitle.add("The Inimitable Jeeves");
-        mAuthor.add("P.G. Wodehouse"); mTitle.add("Leave it to Psmith");
-        mAuthor.add("Viginia Woolf"); mTitle.add("Jacob's Room");
-
-
-        recyclerView.setAdapter(recyclerViewAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));*/
